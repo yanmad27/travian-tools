0bf5e5ef2d9c0543d930c4eb1b85cf5fb0e04e0b
DoanPhan <yanmad27@gmail.com>
2025-04-08 11:34:59 +0700

    feat: class

--- raid.js
+++ raid.js
@@ -1,79 +1,126 @@
+class Victim {
+    constructor(id, interval) {
+        this.id = id;
+        this.interval = interval;
+        this.victim = document.querySelector(`[data-slot-id="${id}"]`).parentNode.parentNode.parentNode;
+    }
+    reInit() {
+        this.victim = document.querySelector(`[data-slot-id="${this.id}"]`).parentNode.parentNode.parentNode;
+    }
+    getInterval() {
+        return this.interval;
+    }
+    select() {
+        this.reInit();
+        this.victim.querySelector('[class="selection"] input').click();
+        console.log('LOG ~ ' + new Date().toLocaleString() + ' ~ select victim: ', this.getName());
+    }
+    getName() {
+        this.reInit();
+        return this.victim.querySelector('[class="target"] a span').innerHTML;
+    }
+    getPopulation() {
+        this.reInit();
+        return this.victim.querySelector('[class="population"] span').innerHTML;
+    }
+    getDistance() {
+        this.reInit();
+        return this.victim.querySelector('[class="distance"] span').innerHTML;
+    }
+    isRaidding() {
+        this.reInit();
+        return this.victim.querySelector('[class="state"]').children.length !== 0;
+    }
+    isLastRaidFull() {
+        this.reInit();
+        return this.victim.querySelector('[class="lastRaidBounty"] i').className.includes('bounty_full_small');
+    }
+    isLastRaidHalf() {
+        this.reInit();
+        return this.victim.querySelector('[class="lastRaidBounty"] i').className.includes('bounty_half_small');
+    }
+}
+
+class FarmList {
+    constructor(id) {
+        this.farmList = document.querySelector(`[data-list="${id}"]`).parentNode;
+    }
+    getName() {
+        return this.farmList.querySelector('[class="farmListName"] [class="name"]').innerHTML;
+    }
+    triggerRaid() {
+        this.farmList.querySelector('[class="farmListName"] [class="name"]').click();
+        this.farmList.querySelector('button').click();
+    }
+    isCollapsed() {
+        return this.farmList.parentNode.className.includes('collapsed');
+    }
+    toggleCollapse() {
+        this.farmList.querySelector('[class="expandCollapse"]').click();
+    }
+    getCurrentSelectedVictim() {
+        var text = this.farmList.querySelector('button div').innerHTML;
+        var regex = /Bắt đầu&nbsp;\((\d+)\)/;
+        var match = text.match(regex);
+        return parseInt(match[1]);
+    }
+}
+
 async function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function getElementByXpath(path) {
-    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
-
-function selectVictim(victim) {
-    var htmlItem = getElementByXpath(victim.xpathCheckbox);
-    if (!htmlItem.checked) htmlItem.click();
-    console.log('LOG ~ ' + new Date().toLocaleString() + ' ~ select victim: ', victim.name);
-}
-
-function triggerRaid() {
-    // Just make sure the interval is active
-    var activeInterval = getElementByXpath('//*[@id="rallyPointFarmList"]/div[2]/div[3]/div/div[1]/div[2]/div[1]');
-    activeInterval.click();
-
-    var act = getElementByXpath('//*[@id="rallyPointFarmList"]/div[2]/div[3]/div/div[1]/button');
-    var s = act.children[0].innerHTML;
-    var regex = /Bắt đầu&nbsp;\((\d+)\)/;
-    var match = s.match(regex);
-    var count = Number(match[1]);
-    if (count <= victims.length) {
-        console.log('LOG ~ ' + new Date().toLocaleString() + ' ~trigger raid');
-        act.click();
-    }
-}
-
-setInterval(triggerRaid, 5000);
-
 var victims = [
     {
+        id: 51231,
         name: 'Mei`s F (-68|-22)',
-        xpathCheckbox: '//*[@id="rallyPointFarmList"]/div[2]/div[3]/div/div[2]/table/tbody/tr[1]/td[1]/label/input',
-        xpathState: '//*[@id="rallyPointFarmList"]/div[2]/div[3]/div/div[2]/table/tbody/tr[1]/td[2]',
         interval: 45,
         active: true,
     },
     {
+        id: 51232,
         name: 'Lalala`s F (-67|-27)',
-        xpathCheckbox: '//*[@id="rallyPointFarmList"]/div[2]/div[3]/div/div[2]/table/tbody/tr[2]/td[1]/label/input',
-        xpathState: '//*[@id="rallyPointFarmList"]/div[2]/div[3]/div/div[2]/table/tbody/tr[2]/td[2]',
         interval: 30,
         active: true,
     },
     {
+        id: 51433,
         name: '02.Camap (-54|-30)',
-        xpathCheckbox: '//*[@id="rallyPointFarmList"]/div[2]/div[3]/div/div[2]/table/tbody/tr[3]/td[1]/label/input',
-        xpathState: '//*[@id="rallyPointFarmList"]/div[2]/div[3]/div/div[2]/table/tbody/tr[3]/td[2]',
         interval: 30,
         active: true,
     },
     {
+        id: 51230,
         name: 'Làng của abcd (-59|-42)',
-        xpathCheckbox: '//*[@id="rallyPointFarmList"]/div[2]/div[3]/div/div[2]/table/tbody/tr[4]/td[1]/label/input',
-        xpathState: '//*[@id="rallyPointFarmList"]/div[2]/div[3]/div/div[2]/table/tbody/tr[4]/td[2]',
         interval: 30,
         active: true,
     },
     {
+        id: 46845,
         name: 'Hopeful`s F (-54|-36)',
-        xpathCheckbox: '//*[@id="rallyPointFarmList"]/div[2]/div[3]/div/div[2]/table/tbody/tr[5]/td[1]/label/input',
-        xpathState: '//*[@id="rallyPointFarmList"]/div[2]/div[3]/div/div[2]/table/tbody/tr[5]/td[2]',
         interval: 30,
         active: true,
     },
 ];
 
-for (let i = 0; i < victims.length; i++) {
-    if (!victims[i].active) continue;
-    (async function () {
-        let victim = victims[i];
-        while (getElementByXpath(victim.xpathState).children.length !== 0) await sleep(5000);
-        selectVictim(victim);
-        setInterval(() => selectVictim(victim), (victim.interval * 60 + 15) * 1000);
-    })();
+function main() {
+    var farmList = new FarmList(1732);
+    setInterval(() => {
+        if (farmList.isCollapsed()) farmList.toggleCollapse();
+        if (farmList.getCurrentSelectedVictim() <= victims.length) farmList.triggerRaid();
+    }, 5000);
+
+    for (let i = 0; i < victims.length; i++) {
+        if (!victims[i].active) continue;
+        console.log('LOG ~ ' + new Date().toLocaleString() + ' ~ start victim: ', victims[i].name, ', after: ', i, ' minutes');
+        (async function () {
+            await sleep(i * 60 * 1 * 1000);
+            let victim = new Victim(victims[i].id, victims[i].interval);
+            while (victim.isRaidding()) await sleep(5000);
+            victim.select();
+            setInterval(() => victim.select(), victim.getInterval() * 60 * 1000);
+        })();
+    }
 }
+
+main();
