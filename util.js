b1e79a4c4574966e63c84cb922b47d34ed7af48b
DoanPhan <yanmad27@gmail.com>
2025-04-13 11:55:18 +0700

    feat: sum res in the day

--- util.js
+++ util.js
@@ -1,8 +1,10 @@
 function getSumRes() {
+    var res = document.querySelectorAll('[class="averageRaidBounty"] [class="value"]');
     var sum = 0;
-    a.forEach((t) => {
-        var number = t.innerText.replace(/\.|\u202D|\u202C/g, '');
-        sum += Number(number);
+    res.forEach((cur) => {
+        sum += Number(cur.innerText.replace(/\.|\u202D|\u202C/g, ''));
     });
     console.log(sum);
 }
+
+getSumRes();
