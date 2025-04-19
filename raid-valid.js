47dd3d4e319f64ed96b51f464ca80452bb81af40
DoanPhan <yanmad27@gmail.com>
2025-04-19 12:24:24 +0700

    chore: update manifest

---
+++ raid-valid.js
@@ -0,0 +1,416 @@
+const activeVictims = new Map([
+	[
+		62034,
+		{
+			id: 62034,
+			interval: 10,
+			intervalId: 3953,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="62034"]',
+			},
+		},
+	],
+	[
+		61851,
+		{
+			id: 61851,
+			interval: 10,
+			intervalId: 3993,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="61851"]',
+			},
+		},
+	],
+	[
+		70221,
+		{
+			id: 70221,
+			interval: 10,
+			intervalId: 214,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="70221"]',
+			},
+		},
+	],
+	[
+		55464,
+		{
+			id: 55464,
+			interval: 15,
+			intervalId: 1121,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="55464"]',
+			},
+		},
+	],
+	[
+		62318,
+		{
+			id: 62318,
+			interval: 10,
+			intervalId: 4116,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="62318"]',
+			},
+		},
+	],
+	[
+		43830,
+		{
+			id: 43830,
+			interval: 10,
+			intervalId: 4126,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="43830"]',
+			},
+		},
+	],
+	[
+		65832,
+		{
+			id: 65832,
+			interval: 10,
+			intervalId: 4163,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="65832"]',
+			},
+		},
+	],
+	[
+		62443,
+		{
+			id: 62443,
+			interval: 15,
+			intervalId: 5686,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="62443"]',
+			},
+		},
+	],
+	[
+		62444,
+		{
+			id: 62444,
+			interval: 15,
+			intervalId: 5721,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="62444"]',
+			},
+		},
+	],
+	[
+		70233,
+		{
+			id: 70233,
+			interval: 10,
+			intervalId: 4272,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="70233"]',
+			},
+		},
+	],
+	[
+		69810,
+		{
+			id: 69810,
+			interval: 30,
+			intervalId: 9853,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="69810"]',
+			},
+		},
+	],
+	[
+		69807,
+		{
+			id: 69807,
+			interval: 30,
+			intervalId: 9881,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="69807"]',
+			},
+		},
+	],
+	[
+		51230,
+		{
+			id: 51230,
+			interval: 10,
+			intervalId: 4379,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="51230"]',
+			},
+		},
+	],
+	[
+		46845,
+		{
+			id: 46845,
+			interval: 10,
+			intervalId: 4406,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="46845"]',
+			},
+		},
+	],
+	[
+		77006,
+		{
+			id: 77006,
+			interval: 10,
+			intervalId: 5624,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="77006"]',
+			},
+		},
+	],
+	[
+		77012,
+		{
+			id: 77012,
+			interval: 10,
+			intervalId: 9655,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="77012"]',
+			},
+		},
+	],
+	[
+		77016,
+		{
+			id: 77016,
+			interval: 10,
+			intervalId: 10243,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="77016"]',
+			},
+		},
+	],
+	[
+		77019,
+		{
+			id: 77019,
+			interval: 10,
+			intervalId: 10853,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="77019"]',
+			},
+		},
+	],
+	[
+		77022,
+		{
+			id: 77022,
+			interval: 10,
+			intervalId: 11104,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="77022"]',
+			},
+		},
+	],
+	[
+		77023,
+		{
+			id: 77023,
+			interval: 10,
+			intervalId: 12038,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="77023"]',
+			},
+		},
+	],
+	[
+		77024,
+		{
+			id: 77024,
+			interval: 10,
+			intervalId: 12415,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="77024"]',
+			},
+		},
+	],
+	[
+		48393,
+		{
+			id: 48393,
+			interval: 10,
+			intervalId: 64641,
+			attempts: 0,
+			maxAttempts: 5,
+			baseElement: {
+				selector: '[data-slot-id="48393"]',
+			},
+		},
+	],
+])
+const farmLists = [
+	{
+		id: 1817,
+		victims: [
+			{
+				id: 62034,
+				name: 'Azamat`s village',
+				interval: 10,
+				active: true,
+			},
+			{
+				id: 61851,
+				name: 'Lalala`s F',
+				interval: 10,
+				active: true,
+			},
+			{
+				id: 70221,
+				name: 'Baki',
+				interval: 10,
+				active: true,
+			},
+			{
+				id: 55464,
+				name: 'An nghĩa đường',
+				interval: 15,
+				active: true,
+			},
+			{
+				id: 62318,
+				name: 'shoes19944116',
+				interval: 10,
+				active: true,
+			},
+			{
+				id: 43830,
+				name: 'ab1` F',
+				interval: 10,
+				active: true,
+			},
+			{
+				id: 65832,
+				name: 'Dorf von Djimmy98',
+				interval: 10,
+				active: true,
+			},
+			{
+				id: 62443,
+				name: 'Mr.Piet 01',
+				interval: 15,
+				active: true,
+			},
+			{
+				id: 62444,
+				name: 'Mr.Piet 00',
+				interval: 15,
+				active: true,
+			},
+			{
+				id: 77006,
+				name: 'Vu Thanh',
+				interval: 10,
+				active: true,
+			},
+			{
+				id: 48393,
+				name: 'Ly ly',
+				interval: 10,
+				active: true,
+			},
+			{
+				id: 70233,
+				name: 'harry83820`s village',
+				interval: 10,
+				active: true,
+			},
+			{
+				id: 69810,
+				name: '.',
+				interval: 30,
+				active: true,
+			},
+			{
+				id: 69807,
+				name: 'A',
+				interval: 30,
+				active: true,
+			},
+		],
+	},
+	{
+		id: 1732,
+		victims: [
+			{
+				id: 61858,
+				name: '02Camap',
+				interval: 10,
+				active: false,
+			},
+			{
+				id: 51230,
+				name: 'Làng của abcd',
+				interval: 10,
+				active: true,
+			},
+			{
+				id: 46845,
+				name: 'Hopeful`s F',
+				interval: 10,
+				active: true,
+			},
+		],
+	},
+]
+
+const victims = farmLists.flatMap((fl) => fl.victims)
+
+victims.forEach((v) => {
+	const baseInterval = activeVictims?.get(v.id)?.interval || 0
+	console.log(v.id, v.name.padEnd(40), `${v.interval}`.padStart(2), `${baseInterval}`.padStart(2), `${baseInterval === v.interval ? '✅' : '❌'}`)
+})
+
+activeVictims.forEach((v) => {
+	const victim = victims.find((victim) => victim.id === v.id)
+	if (!victim) {
+		console.log(v.id, v.interval, '❌')
+	}
+})
