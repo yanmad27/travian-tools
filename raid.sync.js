406e051b8bed75b11b3c6facf45b10000a5ceda9
DoanPhan <yanmad27@gmail.com>
2025-05-01 09:57:12 +0700

    chore: update farmlist

--- raid.sync.js
+++ raid.sync.js
@@ -1,25 +1,24 @@
-var id = 1817
-var farmlist = document.querySelector(`[data-list="${id}"]`).parentElement.parentElement.querySelector('tbody')
+function getFarms(id) {
+	const farmlist = document.querySelector(`[data-list="${id}"]`).parentElement.parentElement.querySelector('tbody')
 
-var farms = []
-farmlist.childNodes.forEach((child, index) => {
-	if (index === farmlist.childNodes.length - 1) return
-	var name = child.querySelector('[class="target"] span').innerHTML
-	if (name.includes('coordinate')) {
-		name =
-			'Oasis ' +
-			child.querySelector('[class="target"] [class="coordinateX"]').innerHTML +
-			'|' +
-			child.querySelector('[class="target"] [class="coordinateY"]').innerHTML 
-	}
-	var item = {
-    no:index+1,
-		id: Number(child.querySelector('[class="selection"] input').getAttribute('data-slot-id')),
-		interval: 10,
-		active: true,
-		name: name,
-	}
-	farms.push(item)
-})
+	const farms = []
+	farmlist.childNodes.forEach((child, index) => {
+		if (index === farmlist.childNodes.length - 1) return
+		let name = child.querySelector('[class="target"] span').innerHTML
+		if (name.includes('coordinate')) {
+			name = `Oasis ${child.querySelector('[class="target"] [class="coordinateX"]').innerHTML}|${child.querySelector('[class="target"] [class="coordinateY"]').innerHTML}`
+		}
+		const item = {
+			no: index + 1,
+			id: Number(child.querySelector('[class="selection"] input').getAttribute('data-slot-id')),
+			interval: 10,
+			active: true,
+			name: name,
+		}
+		farms.push(item)
+	})
+	return farms
+}
 
-console.log(farms)
+console.log(getFarms(1817))
+console.log(getFarms(2291))
