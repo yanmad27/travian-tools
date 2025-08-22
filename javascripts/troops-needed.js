83a35db3152420a44b9f064171d9b180462c2cdf
DoanPhan <yanmad27@gmail.com>
2025-08-23 01:55:47 +0700

    chore: add prettier and update base interval

--- javascripts/troops-needed.js
+++ javascripts/troops-needed.js
@@ -1,15 +1,15 @@
-const troopneededkey = 'troopsNeeded'
-const troopsOverviewkey = 'troopsOverview'
+const troopneededkey = 'troopsNeeded';
+const troopsOverviewkey = 'troopsOverview';
 
 const renderResult = (villageList, troopsOverview) => {
-	const troopsImgPath = 'https://cdn.legends.travian.com/gpack/200.13/img_ltr/global/units/hun/icon/hun_small.png'
+	const troopsImgPath = 'https://cdn.legends.travian.com/gpack/200.13/img_ltr/global/units/hun/icon/hun_small.png';
 	// const interval = Number(document.getElementById('interval').value)
-	const result = document.getElementById('troops-needed-result')
-	result.innerHTML = ''
+	const result = document.getElementById('troops-needed-result');
+	result.innerHTML = '';
 	for (const village of villageList) {
-		const villageName = village.name
-		const tsLevel = village.tsLevel
-		const farmList = village.farmList
+		const villageName = village.name;
+		const tsLevel = village.tsLevel;
+		const farmList = village.farmList;
 		const sumTroops = {
 			t1: 0,
 			t2: 0,
@@ -18,7 +18,7 @@ const renderResult = (villageList, troopsOverview) => {
 			t5: 0,
 			t6: 0,
 			t7: 0,
-		}
+		};
 		const sumTroopsAll = {
 			t1: 0,
 			t2: 0,
@@ -27,13 +27,13 @@ const renderResult = (villageList, troopsOverview) => {
 			t5: 0,
 			t6: 0,
 			t7: 0,
-		}
+		};
 		for (const farm of farmList) {
-			const victimList = farm.victimList
+			const victimList = farm.victimList;
 			for (const victim of victimList) {
 				for (const [key, value] of Object.entries(victim.troops)) {
-					sumTroopsAll[key] += value
-					if (!victim.isDisabled) sumTroops[key] += value
+					sumTroopsAll[key] += value;
+					if (!victim.isDisabled) sumTroops[key] += value;
 				}
 			}
 		}
@@ -42,33 +42,33 @@ const renderResult = (villageList, troopsOverview) => {
                 <div class="farm-name" style="margin-bottom: 8px; font-weight: bold;">${villageName} - <span style="font-size: 12px; color: #999;">TS Lvl: ${tsLevel || 0}</span></div>
                 <div class="farm-troops" style="display: flex; align-items: center; gap: 8px; padding-left: 12px;">
                   ${Object.entries(sumTroops)
-					.sort(([key1], [key2]) => {
-						const order = [ 't2', 't3', 't4', 't5', 't6', 't7','t1',]
-						return order.indexOf(key1) - order.indexOf(key2)
-					})
-					.map(([key, value]) => {
-							if (value === 0|| value==='0') return ''
-							// if (key !== 't4' && key !== 't5' && key !== 't6') return ''
-							// if (value === 0) return ''
-							const backgrounPosition = {
-								t1: '0 0',
-								t2: '0 -16px',
-								t3: '0 -32px',
-								t4: '0 -48px',
-								t5: '0 -64px',
-								t6: '0 -80px',
-								t7: '0 -96px',
-							}
-							const backgroundPosition = backgrounPosition[key] || '0 0'
-							const ratio = Number(value) / Number(troopsOverview[villageName]?.[key] || 1)
-							let color = 'black'
-							if (ratio > 0.95) color = 'red'
-							if (ratio <= 0.95) color = 'orange'
-							if (ratio <= 0.9) color = 'green'
-							const style1 = `color: ${color};`
-							const style2 = `color: #999;`
+										.sort(([key1], [key2]) => {
+											const order = ['t2', 't3', 't4', 't5', 't6', 't7', 't1'];
+											return order.indexOf(key1) - order.indexOf(key2);
+										})
+										.map(([key, value]) => {
+											if (value === 0 || value === '0') return '';
+											// if (key !== 't4' && key !== 't5' && key !== 't6') return ''
+											// if (value === 0) return ''
+											const backgrounPosition = {
+												t1: '0 0',
+												t2: '0 -16px',
+												t3: '0 -32px',
+												t4: '0 -48px',
+												t5: '0 -64px',
+												t6: '0 -80px',
+												t7: '0 -96px',
+											};
+											const backgroundPosition = backgrounPosition[key] || '0 0';
+											const ratio = Number(value) / Number(troopsOverview[villageName]?.[key] || 1);
+											let color = 'black';
+											if (ratio > 0.95) color = 'red';
+											if (ratio <= 0.95) color = 'orange';
+											if (ratio <= 0.9) color = 'green';
+											const style1 = `color: ${color};`;
+											const style2 = `color: #999;`;
 
-							return `
+											return `
               <div style="display: flex; align-items: center; gap: 4px;">
                 <div style="background-image: url(${troopsImgPath});width: 16px;height: 16px;display: inline-block;vertical-align: bottom;background-position: ${backgroundPosition};"></div>
                 <div style="min-width: 75px; max-width: 75px;">
@@ -77,19 +77,19 @@ const renderResult = (villageList, troopsOverview) => {
                   <span style="${style2}">/${troopsOverview[villageName]?.[key]} </span>
                 </div>
               </div>
-              `
-						})
-						.join('')}
+              `;
+										})
+										.join('')}
                 </div>
-              </div>`
+              </div>`;
 	}
-}
+};
 function saveToStorage(data) {
-	chrome.storage.local.set({ [troopneededkey]: data })
+	chrome.storage.local.set({ [troopneededkey]: data });
 }
 
 function saveToStorage2(data) {
-	chrome.storage.local.set({ [troopsOverviewkey]: data }, () => {})
+	chrome.storage.local.set({ [troopsOverviewkey]: data }, () => {});
 }
 
 const getTroopNeeded = () => {
@@ -99,14 +99,14 @@ const getTroopNeeded = () => {
 				{
 					target: { tabId: tabs[0].id },
 					function: () => {
-						const BASE_INTERVAL = 4.9
-						const SLOWEST_SPEED = 28
+						const BASE_INTERVAL = 5.45;
+						const SLOWEST_SPEED = 28;
 
 						const getTroops = (totalLoop, troopsElem) => {
 							const getTroops = (selector) => {
-								const cnt = Number(troopsElem.querySelector(selector)?.parentNode.querySelector('.value').innerHTML)
-								return isNaN(cnt) ? 0 : cnt
-							}
+								const cnt = Number(troopsElem.querySelector(selector)?.parentNode.querySelector('.value').innerHTML);
+								return isNaN(cnt) ? 0 : cnt;
+							};
 							return {
 								t1: getTroops('i.t1') * totalLoop,
 								t2: getTroops('i.t2') * totalLoop,
@@ -115,71 +115,71 @@ const getTroopNeeded = () => {
 								t5: getTroops('i.t5') * totalLoop,
 								t6: getTroops('i.t6') * totalLoop,
 								t7: getTroops('i.t7') * totalLoop,
-							}
-						}
-						const villageListElems = document.querySelectorAll('.villageWrapper ')
-						const villageList = []
+							};
+						};
+						const villageListElems = document.querySelectorAll('.villageWrapper ');
+						const villageList = [];
 						for (const villageListElem of villageListElems) {
-							const villageName = villageListElem.querySelector('.villageName').innerHTML
+							const villageName = villageListElem.querySelector('.villageName').innerHTML;
 							const getTSLevel = (villageName) => {
 								const tsLevel = {
 									N01: 18,
 									F01: 10,
 									F02: 10,
 									F03: 10,
-								}
-								return tsLevel[villageName] || 0
-							}
-							const tsLevel = getTSLevel(villageName)
-							const farmListElems = villageListElem.querySelectorAll('.farmListWrapper')
-							const farmList = []
+								};
+								return tsLevel[villageName] || 0;
+							};
+							const tsLevel = getTSLevel(villageName);
+							const farmListElems = villageListElem.querySelectorAll('.farmListWrapper');
+							const farmList = [];
 							for (const farmListElem of farmListElems) {
-								const farmListName = farmListElem.querySelector('.farmListName .name').innerHTML
-								const victimListElem = farmListElem.querySelectorAll('tr.slot')
-								const victimList = []
+								const farmListName = farmListElem.querySelector('.farmListName .name').innerHTML;
+								const victimListElem = farmListElem.querySelectorAll('tr.slot');
+								const victimList = [];
 								for (const victimElem of victimListElem) {
-									const isDisabled = victimElem.getAttribute('class')?.includes('disabled')
-									const id = victimElem.querySelector('[class="selection"] input').getAttribute('data-slot-id')
-									const distance = Number(victimElem.querySelector('.distance span').innerHTML)
-									const loopWithin20Field = (2 * 60 * Math.min(distance, 20)) / SLOWEST_SPEED // 85.7
-									const loopOutside20Field = (2 * 60 * Math.max(distance - 20, 0)) / (SLOWEST_SPEED * (1 + tsLevel * 0.2))
-									const totalLoop = (loopWithin20Field + loopOutside20Field) / BASE_INTERVAL
-									const ceilTotalLoop = Math.ceil(totalLoop)
-									const troops = getTroops(ceilTotalLoop, victimElem.querySelector('td.troops div'))
+									const isDisabled = victimElem.getAttribute('class')?.includes('disabled');
+									const id = victimElem.querySelector('[class="selection"] input').getAttribute('data-slot-id');
+									const distance = Number(victimElem.querySelector('.distance span').innerHTML);
+									const loopWithin20Field = (2 * 60 * Math.min(distance, 20)) / SLOWEST_SPEED; // 85.7
+									const loopOutside20Field = (2 * 60 * Math.max(distance - 20, 0)) / (SLOWEST_SPEED * (1 + tsLevel * 0.2));
+									const totalLoop = (loopWithin20Field + loopOutside20Field) / BASE_INTERVAL;
+									const ceilTotalLoop = Math.ceil(totalLoop);
+									const troops = getTroops(ceilTotalLoop, victimElem.querySelector('td.troops div'));
 									const item = {
 										id,
 										distance,
 										troops,
 										isDisabled,
-									}
-									victimList.push(item)
+									};
+									victimList.push(item);
 								}
 								farmList.push({
 									name: farmListName,
 									victimList,
-								})
+								});
 							}
 							villageList.push({
 								name: villageName,
 								tsLevel,
 								farmList,
-							})
+							});
 						}
 
-						return villageList
+						return villageList;
 					},
 				},
 				(results) => {
-					const villageList = results[0].result
-					if (villageList.length === 0) return
-					saveToStorage(villageList)
-				},
-			)
-		})
+					const villageList = results[0].result;
+					if (villageList.length === 0) return;
+					saveToStorage(villageList);
+				}
+			);
+		});
 	} catch (error) {
-		console.log('LOG ~ getTroopNeeded ~ error:', error)
+		console.log('LOG ~ getTroopNeeded ~ error:', error);
 	}
-}
+};
 
 const getTroopsOverview = () => {
 	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -187,16 +187,16 @@ const getTroopsOverview = () => {
 			{
 				target: { tabId: tabs[0].id },
 				function: () => {
-					if (!document.location.href.includes('/village/statistics/troops')) return
-					const table = document.querySelector('#troops')
-					const tmps = table.querySelectorAll('td.villageName')
-					const troops = {}
+					if (!document.location.href.includes('/village/statistics/troops')) return;
+					const table = document.querySelector('#troops');
+					const tmps = table.querySelectorAll('td.villageName');
+					const troops = {};
 					for (const tmp of tmps) {
-						const row = tmp.parentNode
+						const row = tmp.parentNode;
 						const getTroop = (index) => {
-							return isNaN(Number(row.querySelector(`td:nth-child(${index})`).innerHTML)) ? 0 : Number(row.querySelector(`td:nth-child(${index})`).innerHTML)
-						}
-						const name = row.querySelector('.villageName a').innerHTML
+							return isNaN(Number(row.querySelector(`td:nth-child(${index})`).innerHTML)) ? 0 : Number(row.querySelector(`td:nth-child(${index})`).innerHTML);
+						};
+						const name = row.querySelector('.villageName a').innerHTML;
 						const troop = {
 							t1: getTroop(2),
 							t2: getTroop(3),
@@ -208,33 +208,33 @@ const getTroopsOverview = () => {
 							t8: getTroop(9),
 							t9: getTroop(10),
 							t10: getTroop(11),
-						}
-						troops[name] = troop
+						};
+						troops[name] = troop;
 					}
-					return troops
+					return troops;
 				},
 			},
 			(results) => {
-				const troops = results[0].result
-				if (troops.length === 0) return
-				saveToStorage2(troops)
-			},
-		)
-	})
-}
+				const troops = results[0].result;
+				if (troops.length === 0) return;
+				saveToStorage2(troops);
+			}
+		);
+	});
+};
 document.addEventListener('DOMContentLoaded', () => {
-	getTroopNeeded()
-	getTroopsOverview()
+	getTroopNeeded();
+	getTroopsOverview();
 	chrome.storage.local.get([troopneededkey, troopsOverviewkey], (result) => {
-		const villageList = result[troopneededkey] || []
-		const troopsOverview = result[troopsOverviewkey] || {}
-		renderResult(villageList, troopsOverview)
-	})
+		const villageList = result[troopneededkey] || [];
+		const troopsOverview = result[troopsOverviewkey] || {};
+		renderResult(villageList, troopsOverview);
+	});
 	setTimeout(() => {
 		chrome.storage.local.get([troopneededkey, troopsOverviewkey], (result) => {
-			const villageList = result[troopneededkey] || []
-			const troopsOverview = result[troopsOverviewkey] || {}
-			renderResult(villageList, troopsOverview)
-		})
-	}, 500)
-})
+			const villageList = result[troopneededkey] || [];
+			const troopsOverview = result[troopsOverviewkey] || {};
+			renderResult(villageList, troopsOverview);
+		});
+	}, 500);
+});
